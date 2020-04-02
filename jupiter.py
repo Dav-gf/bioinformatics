423c8b2bfcd8937e7d0a7dad2a48c4c5efd1550c
David <dav2345@gmail.com>
2020-04-02 09:50:29 -0500

    Added a function that calls fastqc on two given fastq files

---
+++ jupiter.py
@@ -0,0 +1,14 @@
+
+
+
+import sys
+import subprocess
+
+    
+print(forward,reverse)
+
+def run_fastqc(forward_read,reverse_reads)
+
+subprocess.run("fastqc {} {}" .format(forward_read,reverse_reads),shell=True)
+
+forward,reverse = sys.argv[1:]
